fb2d6055b60e426f020116d0e864339671af6c6e
Kozlovsk1 <135413152+Kozlovsk1@users.noreply.github.com>
2025-04-22 23:40:58 +0200

    Update telegrambotcode.py

--- telegrambotcode.py
+++ telegrambotcode.py
@@ -5,9 +5,13 @@ from telegram import Bot, Update
 from flask import Flask, request
 from telegram.ext import Updater, CommandHandler, CallbackContext
 import threading
+import os
+
+
 
 # --- Налаштування ---
-
+TOKEN = os.getenv("TOKEN")
+CHAT_ID = os.getenv("CHAT_ID")  # Твій Telegram ID
 TRACKING_URL = 'https://www.ups.com/track?loc=en_US&tracknum=1ZA0Y3156822543252'
 THUM_IO_URL = f'https://image.thum.io/get/width/1000/{TRACKING_URL}'
 
